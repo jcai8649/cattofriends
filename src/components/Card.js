aac69920217a87211cdf93d3aaca78ae47fb5cfa
Jackson Cai <jcai8649@gmail.com>
2020-06-22 12:40:09 -0700

    Update package.json

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 const Card = ({name, email, id}) => {
     return (
-        <div className = "tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+        <div className = "tc bg-light-blue dib br3 pa3 ma2 grow bw2 shadow-5">
             <img src={`https://robohash.org/${id}?set=set4`} alt="cat"/>
             <div>
                 <h2>{name}</h2>
                 <p>{email}</p>
             </div>
-        </div>
+     ``   </div>
     )
 }
 
