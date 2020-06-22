1f6db064248efe6f225b5d3d8127a072ac0ecea0
Jackson Cai <jcai8649@gmail.com>
2020-06-22 12:40:44 -0700

    Updates

---
+++ precache-manifest.f2cb724b1edb1d291dfd876c83584561.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c71c6e4184c847636aaf7b50e4fed5a2",
+    "url": "/jcai8649/cattofriends/index.html"
+  },
+  {
+    "revision": "80452ae01dc069a51b6e",
+    "url": "/jcai8649/cattofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "702a116aea60282a2dc5",
+    "url": "/jcai8649/cattofriends/static/css/main.a7bee02a.chunk.css"
+  },
+  {
+    "revision": "80452ae01dc069a51b6e",
+    "url": "/jcai8649/cattofriends/static/js/2.ff41af6d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jcai8649/cattofriends/static/js/2.ff41af6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "702a116aea60282a2dc5",
+    "url": "/jcai8649/cattofriends/static/js/main.228525bf.chunk.js"
+  },
+  {
+    "revision": "8537ea21cbe7994cbf92",
+    "url": "/jcai8649/cattofriends/static/js/runtime-main.69023afe.js"
+  },
+  {
+    "revision": "385354ed7b2c6e863bea32e725ae35a8",
+    "url": "/jcai8649/cattofriends/static/media/ka1.385354ed.woff"
+  }
+]);
